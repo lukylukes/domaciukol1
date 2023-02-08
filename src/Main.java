2ce139ba19e8addd9d6150632b490774ec6db8ee
lukylukes <lukca.lukes@seznam.cz>
2023-02-08 18:17:32 +0100

    Oprava publikování úkolu.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // pojmenování proměnných
+        String nameAndSurname;
+        LocalDate dateOfBirth;
+        int numberOfNegotiateContract;
+        double soldedWeight;
+        String town;
+        String licensePlate;
+        double consumption;
+        String ipAdress;
+
+        soldedWeight = 2000;
+        numberOfNegotiateContract = 2;
+
+        double averageWeight = soldedWeight/numberOfNegotiateContract;
+
+        System.out.println("Average weight per one contract is 1000 ton.");
     }
 }
